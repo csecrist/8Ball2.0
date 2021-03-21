2fbbf4880c50f8487cdae466adff30d5da066b14
csecrist <caitykat15@gmail.com>
2021-03-21 11:06:31 -0700

    v2.0.1
    Resolved bugs with printf and uninitialized variables.

--- 8Ball2.0/8Ball2.0.cpp
+++ 8Ball2.0/8Ball2.0.cpp
@@ -1,54 +1,55 @@
 #include <time.h>
 #include <stdlib.h>  
+#include <stdio.h>
 
 // The following will give us the randomized classic 8 Ball response
-const char * GetClassicAnswer(int index)
+const char* GetClassicAnswer(int index)
 {
-  switch(index) {
+    switch (index) {
     case 0:
-      return "It is certain";
+        return "It is certain";
     case 1:
-      return "It is decidedly so";
+        return "It is decidedly so";
     case 2:
-      return "Without a doubt";
+        return "Without a doubt";
     case 3:
-      return "Yes - definitely";
+        return "Yes - definitely";
     case 4:
-      return "You may rely on it";
+        return "You may rely on it";
     case 5:
-      return "As I see it, yes";
+        return "As I see it, yes";
     case 6:
-      return "Most likely";
+        return "Most likely";
     case 7:
-      return "Outlook good";
+        return "Outlook good";
     case 8:
-      return "Yes";
+        return "Yes";
     case 9:
-      return "Signs point to yes";
+        return "Signs point to yes";
     case 10:
-      return "Reply hazy, try again";
+        return "Reply hazy, try again";
     case 11:
-      return "Ask again later";
+        return "Ask again later";
     case 12:
-      return "Better not tell you now";
+        return "Better not tell you now";
     case 13:
-      return "Cannot predict now";
+        return "Cannot predict now";
     case 14:
-      return "Concentrate and ask again";
+        return "Concentrate and ask again";
     case 15:
-      return "Don't count on it";
+        return "Don't count on it";
     case 16:
-      return "My reply is no";
+        return "My reply is no";
     case 17:
-      return "My sources say no";
+        return "My sources say no";
     case 18:
-      return "Outlook not so good";
+        return "Outlook not so good";
     case 19:
-      return "Very doubtful";
+        return "Very doubtful";
 
     default:
-      return "No";
-  }
+        return "No";
+    }
 }
 
 //Generates a random number between 0 and max
@@ -89,7 +90,7 @@ int main()
     printf("Welcome!\n");
 
     printf("Hello User, what is it you seek the answer to? (Max 1024 characters)\n"); // prompt user for question
-    char *userQuestion;
+    char* userQuestion = new char[256];
     scanf("%s", userQuestion);
     // printf("%s\n", GetClassicAnswer(20));
     printf("%s\n", GetClassicAnswer(RandomInteger(20)));
